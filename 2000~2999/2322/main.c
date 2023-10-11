72ce37d8dcccf4a690f1258c6785191371eba33f
김다온 <128460925+kdaon07@users.noreply.github.com>
2023-10-11 23:41:00 +0900

    commit
    2023/10/11

---
+++ 2000~2999/2322/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    char a[3], b[3];
+    scanf("%s %s", a, b);
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            if(a[i]=='A' && b[j]=='A')
+        }
+    }
+    return 0;
+}
